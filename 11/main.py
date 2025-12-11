60ed43fd06d7ba963a0c4fe7e9c8f6838ae1b99f
DotNetCookie <dotnet.cookie@gmail.com>
2025-12-11 09:57:55 +0100

    solve day 11 - part 2

--- 11/main.py
+++ 11/main.py
@@ -4,15 +4,46 @@ with open("11/input.txt") as file:
 flows = {r.split(": ")[0]: r.split(": ")[1].split(" ") for r in rows}
 
 
-def get_path(key):
+def get_path_1(key):
     outs = flows.get(key, [])
     total = 0
     for o in outs:
         if o == "out":
             total += 1
         else:
-            total += get_path(o)
+            total += get_path_1(o)
     return total
 
 
-print("Part 1:", get_path("you"))
+cache = {}
+
+
+def get_path_2(key, path=()):
+    if key not in cache:
+        outs = flows.get(key, [])
+        total_out, total_solve = 0, 0
+        fft, dac = 0, 0
+        for o in outs:
+            if o == "out":
+                total_out += 1
+            else:
+                r = get_path_2(o, path + (key,))
+                total_out += r[0]
+                if key == "dac":
+                    dac += r[0]
+                    fft += r[1]
+                    total_solve += r[1]
+                elif key == "fft":
+                    fft += r[0]
+                    dac += r[2]
+                    total_solve += r[2]
+                else:
+                    fft += r[1]
+                    dac += r[2]
+                    total_solve += r[3]
+        cache[key] = (total_out, fft, dac, total_solve)
+    return cache[key]
+
+
+print("Part 1:", get_path_1("you"))
+print("Part 2:", get_path_2("svr")[3])
