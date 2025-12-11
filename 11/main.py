df16d398147039774eeef551fe2938562e30e5f7
DotNetCookie <dotnet.cookie@gmail.com>
2025-12-11 08:32:01 +0100

    solve day 11 - part 1

--- 11/main.py
+++ 11/main.py
@@ -1,2 +1,18 @@
-with open("11/test_input.txt") as file:
-    pass
+with open("11/input.txt") as file:
+    rows = file.read().split("\n")
+
+flows = {r.split(": ")[0]: r.split(": ")[1].split(" ") for r in rows}
+
+
+def get_path(key):
+    outs = flows.get(key, [])
+    total = 0
+    for o in outs:
+        if o == "out":
+            total += 1
+        else:
+            total += get_path(o)
+    return total
+
+
+print("Part 1:", get_path("you"))
