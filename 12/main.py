628cfc68140964aba350cffb3a550387a1deac4f
DotNetCookie <dotnet.cookie@gmail.com>
2025-12-12 09:25:33 +0100

    solve day 12

--- 12/main.py
+++ 12/main.py
@@ -1,2 +1,22 @@
-with open("12/test_input.txt") as file:
-    pass
+with open("12/input.txt") as file:
+    rows = file.read().split("\n")
+
+shapes, shape = [], []
+for row in rows[:30]:
+    if row == "":
+        shapes.append(len([y for x in shape for y in x if y == "#"]))
+        shape = []
+    elif ":" not in row:
+        shape.append(row)
+
+total = 0
+for row in rows[30:]:
+    dimensions, shapes_needed = row.split(": ")
+    width, height = dimensions.split("x")
+    area_needed = sum(
+        [shapes[i] * c for i, c in enumerate(map(int, shapes_needed.split(" ")))]
+    )
+    if int(width) * int(height) - area_needed >= 0:
+        total += 1
+
+print(total)
